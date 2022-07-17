93bd3c2ada356c6c208caa9e55fdfcc62e912c57
brian-indi-com <brian@indi.com>
2022-07-17 19:19:55 -0400

    refactord

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"github.com/becunningham/bookings/pkg/config"
-	"github.com/becunningham/bookings/pkg/handlers"
+	"github.com/becunningham/bookings/internal/config"
+	"github.com/becunningham/bookings/internal/handlers"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"net/http"
@@ -20,6 +20,8 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/generals-quarters", handlers.Repo.Generals)
 	mux.Get("/majors-suite", handlers.Repo.Majors)
 	mux.Get("/search-availability", handlers.Repo.Availability)
+	mux.Post("/search-availability", handlers.Repo.PostAvailability)
+	mux.Post("/search-availability-json", handlers.Repo.AvailabilityJSON)
 	mux.Get("/contact", handlers.Repo.Contact)
 
 	mux.Get("/make-reservation", handlers.Repo.Reservation)
--- internal/render/render.go
+++ internal/render/render.go
@@ -2,8 +2,9 @@ package render
 
 import (
 	"bytes"
-	"github.com/becunningham/bookings/pkg/config"
-	"github.com/becunningham/bookings/pkg/models"
+	"github.com/becunningham/bookings/internal/config"
+	"github.com/becunningham/bookings/internal/models"
+	"github.com/justinas/nosurf"
 	"html/template"
 	"log"
 	"net/http"
@@ -16,12 +17,13 @@ var app *config.AppConfig
 func NewTemplates(a *config.AppConfig) {
 	app = a
 }
-func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+func AddDefaultData(td *models.TemplateData, r *http.Request) *models.TemplateData {
+	td.CSRFToken = nosurf.Token(r)
 	return td
 }
 
 // RenderTemplate renders a template
-func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
+func RenderTemplate(w http.ResponseWriter, r *http.Request, tmpl string, td *models.TemplateData) {
 
 	// get the template cache from the app config
 	var tc map[string]*template.Template
@@ -39,7 +41,7 @@ func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData)
 
 	buf := new(bytes.Buffer)
 
-	td = AddDefaultData(td)
+	td = AddDefaultData(td, r)
 
 	err := t.Execute(buf, td)
 	if err != nil {
